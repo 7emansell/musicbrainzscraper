62ab43bae805d46fc7c0e612398e034f7f07fdd4
7emansell <73774046+7emansell@users.noreply.github.com>
2021-12-13 14:54:45 -0600

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -5,7 +5,6 @@ import numpy as np
 import sys
 import time
 
-ba_list = []
 area_list = []
 name_list = []
 url_list = []
@@ -13,7 +12,7 @@ og = pd.read_csv("file://localhost/Users/emansell/Desktop/urls.csv")
 urls=og.values.flatten()
 
 for url in urls:
-    time.sleep(0.5) 
+    time.sleep(1) 
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
     name = [x.text for x in soup.select('name')]
@@ -30,17 +29,18 @@ for url in urls:
              
 
 
-# url = "https://musicbrainz.org/ws/2/artist/?query=artist:hall-of-fame"
-# r = requests.get(url)
-# soup = BeautifulSoup(r.text, 'lxml')
-# name = soup.find('name') #[x.text for x in soup.select('name')]
-# area = soup.select('area + name')[0]
-# beginarea = soup.findChild(name)
-# print(name, area.text, beginarea)
+# experimenting with one url:
+    # url = "https://musicbrainz.org/ws/2/artist/?query=artist:hall-of-fame"
+    # r = requests.get(url)
+    # soup = BeautifulSoup(r.text, 'lxml')
+    # name = soup.find('name') #[x.text for x in soup.select('name')]
+    # area = soup.select('area + name')[0]
+    # beginarea = soup.findChild(name)
+    # print(name, area.text, beginarea)
 
 
 #  create dataframe from your lists
 df = pd.DataFrame(list(zip(url_list, name_list, area_list)),
                columns =['url', 'name', 'area'])
 
-df.to_csv('RESULTS>8.csv', index=False, errors='ignore')
+df.to_csv('CRAWLERRESULTS.csv', index=False, errors='ignore')
